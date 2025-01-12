e9fb560cd4b5fa5015245b67deb2f0858c4e336b
JJDataAnalyst-viz <kubajedrych100@gmail.com>
2025-01-12 13:23:57 +0100

    setting exception and logging

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys 
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exec_tb = error_detail.exc_info()
@@ -11,14 +12,12 @@ def error_message_detail(error,error_detail:sys):
     return message_error
 
 
-class AgeException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
     def __str__(self):
         return self.error_message
-    
-
     
 
 
--- src/logger.py
+++ src/logger.py
@@ -5,10 +5,10 @@ from datetime import datetime
 
 
 LOG_FILE = f'%{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.log'
-log_file = os.path.join(os.getcwd(),'src','logs')
+log_file = os.path.join(os.getcwd(),'logs')
 os.makedirs(log_file,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(os.getcwd(),'src','logs',LOG_FILE)
+LOG_FILE_PATH = os.path.join(os.getcwd(),'logs',LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -16,5 +16,3 @@ logging.basicConfig(
     format= "[%(asctime)s] --%(lineno)d--  %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
     )
-
-logging.info('Hello world')
